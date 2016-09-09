91cbcf3c0ece057beb7ec4e5ba2ab78bd840db23
Hung BCP <hungbcp@asiantech.vn>
2016-09-09 14:54:25 +0700

    Update ES6 => function usage
    Add more comments for hack

--- github.js
+++ github.js
@@ -2,12 +2,19 @@ const parse = require('parse-link-header');
 const XMLHttpRequest = require('xhr2');
 const xhr = new XMLHttpRequest();
 
-let cruiser = function (user = process.argv[2], page = 1) {
+/**
+ * @user Read automatically from third argument for user
+ * @page Default is 1 and increase if response has pagination
+ */
+let cruiser = (user = process.argv[2], page = 1) => {
+  // Stop and output usage if an invalid input for username
   if (!user && typeof user !== 'string') {
     console.warn(`Usage:\nGITHUB_USER    A valid GitHub username`);
     process.exit(-1);
   }
 
+  // Construct request with default OAuth applications
+  // You can setup your own app with its ID/Secret key here: https://github.com/settings/applications
   xhr.open(
     'GET',
     'https://api.github.com/users/' + user +
@@ -19,12 +26,16 @@ let cruiser = function (user = process.argv[2], page = 1) {
 
   xhr.onreadystatechange = () => {
     if (xhr.readyState === 4 && xhr.status === 200) {
+      // Output all responsed headers if `--debug` argument found
       if (process.argv[3] === '--debug') {
         console.log(xhr.getAllResponseHeaders() + '\n---\n');
       }
 
+      // Output result
       if (xhr.response.length) {
         xhr.response.map(value => {
+          // Append `/releases.atom` for repo releases feed,
+          // feel free to modify and output anything you want.
           process.stdout.write(`${value.html_url}/releases.atom\n`);
         });
       } else {
@@ -32,9 +43,11 @@ let cruiser = function (user = process.argv[2], page = 1) {
         process.exit(1);
       }
 
+      // Read responsed `link` header for pagination
       let link = xhr.getResponseHeader('link');
       let parsed = parse(link);
 
+      // Only recurisve for more result if next page exists
       if (parsed && parsed.next) {
         cruiser(user, parsed.next.page);
       }
@@ -44,4 +57,5 @@ let cruiser = function (user = process.argv[2], page = 1) {
   xhr.send();
 }
 
+// Do the magic
 cruiser();
